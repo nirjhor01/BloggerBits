e829130398b18b8427bd13de530a6fe83fcfe3de
nirjhor01 <tanveerahmednirjhor0@gmail.com>
2025-04-16 21:40:06 +0600

    Feat: Helper ApiResponse<T> added

---
+++ Helper/ApiResponse.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace BloggerBits.Helper;
+
+public class ApiResponse<T>
+{
+    public bool Success { get; set; }
+    public string Message { get; set; } = string.Empty;
+    public T? Data { get; set; }
+
+    public static ApiResponse<T> Ok(string message, T? data = default) =>
+        new ApiResponse<T> { Success = true, Message = message, Data = data };
+
+    public static ApiResponse<T> Fail(string message, T? data = default) =>
+        new ApiResponse<T> { Success = false, Message = message, Data = data };
+}
+
+
+
