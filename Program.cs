1b2c74ffdf46992011fc03f8635e64cff2627a73
nirjhor01 <tanveerahmednirjhor0@gmail.com>
2025-05-17 00:39:02 +0600

    issue fix

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ builder.Services.AddAutoMapper(typeof(MapperProfile));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
--- obj/Debug/net8.0/BloggerBits.AssemblyInfo.cs
+++ obj/Debug/net8.0/BloggerBits.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BloggerBits")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae870b6f89c0bc7d3a2c28666c2fcc0574f26c1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3397caa7a47302e32054b64290424c7c7ec5fe31")]
 [assembly: System.Reflection.AssemblyProductAttribute("BloggerBits")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BloggerBits")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
